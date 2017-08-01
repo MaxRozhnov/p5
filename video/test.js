b9e4b10e1a0e57a497711001aff7e20585899e37
MaxRozhnov <skymax1998@gmail.com>
2017-08-01 15:54:56 -0400

    minor

--- video/test.js
+++ video/test.js
@@ -28,16 +28,17 @@ function draw() {
     var g2 = prev.pixels[index+1];
     var b2 = prev.pixels[index+2];
 
+    pixels[index+0] = r1;
+    pixels[index+1] = g1;
+    pixels[index+2] = b1;
+
 
 
     if (distSq(r1,g1,b1,r2,g2,b2) > threshold){
-      var x = index % width;
-      var y = floor(index / width);
+
       //  pixels[index+0] = 0;
       //  pixels[index+1] = 0;
       //  pixels[index+2] = 0;
-      stroke(0);
-      point(x,y);
        //pixels[index+3] = 255;
     }else{
         //  pixels[index+0] = 255;
