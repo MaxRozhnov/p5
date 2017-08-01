dcc7d89b3c383bd0d261790e7eedf9f98c795d8c
MaxRozhnov <skymax1998@gmail.com>
2017-08-01 01:27:18 -0400

    minor

--- video/sketch.js
+++ video/sketch.js
@@ -6,7 +6,7 @@ function setup() {
   video = createCapture(VIDEO);
   video.hide();
   prev = createImage(320,240);
-  createCanvas(video.width,video.height);
+  createCanvas(320,240);
 }
 
 function draw() {
@@ -22,23 +22,23 @@ function draw() {
       var g1 = video.pixels[index+1];
       var b1 = video.pixels[index+2];
 
-      var r2 = prev.pixels[index+0];
-      var g2 = prev.pixels[index+1];
-      var b2 = prev.pixels[index+2];
-
-      if (distSq(r1,g1,b1,r2,g2,b2) > 2000){
-        pixels[index+0] = 0;
-        pixels[index+1] = 0;
-        pixels[index+2] = 0;
-        //pixels[index+3] = 255;
-
-      }else{
-        pixels[index+0] = 0;
-        pixels[index+1] = 0;
-        pixels[index+2] = 0;
+      // var r2 = prev.pixels[index+0];
+      // var g2 = prev.pixels[index+1];
+      // var b2 = prev.pixels[index+2];
+      //
+      // if (distSq(r1,g1,b1,r2,g2,b2) > 2000){
+      //   pixels[index+0] = 0;
+      //   pixels[index+1] = 0;
+      //   pixels[index+2] = 0;
+      //   //pixels[index+3] = 255;
+      //
+      // }else{
+        pixels[index+0] = r1;
+        pixels[index+1] = g1;
+        pixels[index+2] = b1;
         pixels[index+3] = 255;
 
-      }
+//      }
 
       prev.pixels[index+0] = r1;
       prev.pixels[index+1] = g1;
@@ -47,6 +47,7 @@ function draw() {
     }
   }
   updatePixels();
+  prev.updatePixels();
 
 }
 
