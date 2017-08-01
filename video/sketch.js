70e5c44755523627fb69ac9f8b9044bb1731dd1a
MaxRozhnov <skymax1998@gmail.com>
2017-08-01 01:30:40 -0400

    minor

--- video/sketch.js
+++ video/sketch.js
@@ -22,23 +22,23 @@ function draw() {
       var g1 = video.pixels[index+1];
       var b1 = video.pixels[index+2];
 
-      // var r2 = prev.pixels[index+0];
-      // var g2 = prev.pixels[index+1];
-      // var b2 = prev.pixels[index+2];
-      //
-      // if (distSq(r1,g1,b1,r2,g2,b2) > 2000){
-      //   pixels[index+0] = 0;
-      //   pixels[index+1] = 0;
-      //   pixels[index+2] = 0;
-      //   //pixels[index+3] = 255;
-      //
-      // }else{
-        pixels[index+0] = r1;
-        pixels[index+1] = g1;
-        pixels[index+2] = b1;
+      var r2 = prev.pixels[index+0];
+      var g2 = prev.pixels[index+1];
+      var b2 = prev.pixels[index+2];
+
+      if (distSq(r1,g1,b1,r2,g2,b2) > 2000){
+        pixels[index+0] = 0;
+        pixels[index+1] = 0;
+        pixels[index+2] = 0;
+        //pixels[index+3] = 255;
+
+      }else{
+        pixels[index+0] = 255;
+        pixels[index+1] = 255;
+        pixels[index+2] = 255;
         pixels[index+3] = 255;
 
-//      }
+      }
 
       prev.pixels[index+0] = r1;
       prev.pixels[index+1] = g1;
