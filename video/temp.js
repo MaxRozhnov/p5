ad3bea7a9eeaa4fa3293ab552df933eecd49fde3
MaxRozhnov <skymax1998@gmail.com>
2017-08-01 14:10:44 -0400

    minor

--- video/temp.js
+++ video/temp.js
@@ -6,30 +6,26 @@ function setup() {
   pixelDensity(1);
   video = createCapture(VIDEO);
   video.size(width/vScale,height/vScale);
-  //video.hide();
+
   prev = createImage(width/vScale,height/vScale);
-  //prev = copy(video,0,0,video.width,video.height,video.width,video.height);
+
   frameRate(30);
 }
 
 function draw() {
   background(255);
   fill(0);
-  //rect(0,0,100,100);
-  // console.log(video.get(0,0));
-  // console.log(prev.get(0,0));
   for (var y = 0; y < video.height; y++){
     for (var x = 0; x < video.width; x++){
         var index = y * video.width + x;
         var col = video.get(x,y);
         var pcol = prev.get(x,y);
 
-
         if (distSq(col[0],col[1],col[2],pcol[0],pcol[1],pcol[2]) > 1000) {
           fill(0);
           noStroke();
           rect(x*vScale,y*vScale,vScale,vScale);
-          //console.log(distSq(col[0],col[1],col[2],pcol[0],pcol[1],pcol[2]));
+
         }else{
 
         }
