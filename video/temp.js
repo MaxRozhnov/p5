084432487de71d62d347619f42b7702f3516dac6
MaxRozhnov <skymax1998@gmail.com>
2017-08-01 13:18:56 -0400

    minor

--- video/temp.js
+++ video/temp.js
@@ -14,18 +14,21 @@ function setup() {
 
 function draw() {
   background(255);
+  console.log(video.get(0,0));
+  console.log(prev.get(0,0))
 
   for (var y = 0; y < video.height; y++){
     for (var x = 0; x < video.width; x++){
         var index = y * video.width + x;
         var col = video.get(x,y);
         var pcol = prev.get(x,y);
-        console.log(distSq(col[0],col[1],col[2],pcol[0],pcol[1],pcol[2]));
+
 
         if (distSq(col[0],col[1],col[2],pcol[0],pcol[1],pcol[2]) > 1000) {
           fill(0);
           noStroke();
           point(x,y);
+          console.log(distSq(col[0],col[1],col[2],pcol[0],pcol[1],pcol[2]));
         }else{
 
         }
