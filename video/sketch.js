5afef52aee05f0ecbc09877b0d98513a28459f4f
MaxRozhnov <skymax1998@gmail.com>
2017-08-01 00:30:50 -0400

    minor

--- video/sketch.js
+++ video/sketch.js
@@ -4,8 +4,19 @@ function setup() {
   //console.log(cameras);
   var video = createCapture(VIDEO);
   video.hide();
+  createCanvas(320,240);
 }
 
 function draw() {
+  video.loadPixels();
+  loadPixels();
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (video.width - x + 1 + (y * video.width))*4;
+      var r = video.pixels[index+0];
+      var g = video.pixels[index+1];
+      var b = video.pixels[index+2];
+  }
+}
 
 }
