baec9e72c4550de486f6c775e2db327fb956cc44
MaxRozhnov <skymax1998@gmail.com>
2017-08-01 14:27:55 -0400

    minor

--- video/test.js
+++ video/test.js
@@ -16,36 +16,36 @@ function draw() {
   video.loadPixels();
   prev.loadPixels()
   loadPixels();
-  for (var y = 0; y < video.height; y++) {
-    for (var x = 0; x < video.width; x++) {
-      var index = (video.width - x + 1 + (y * video.width))*4;
+  for (var index = 0; index < video.pixels.length, index++) {
+    pixels[index] = video.pixels[index];
 
-      var r1 = video.pixels[index+0];
-      var g1 = video.pixels[index+1];
-      var b1 = video.pixels[index+2];
 
-      var r2 = prev.pixels[index+0];
-      var g2 = prev.pixels[index+1];
-      var b2 = prev.pixels[index+2];
-
-      if (distSq(r1,g1,b1,r2,g2,b2) > threshold){
-        pixels[index+0] = 0;
-        pixels[index+1] = 0;
-        pixels[index+2] = 0;
-
-      }else{
-         pixels[index+0] = 255;
-         pixels[index+1] = 255;
-         pixels[index+2] = 255;
-         pixels[index+3] = 255;
-
-      }
-
-      prev.pixels[index+0] = r1;
-      prev.pixels[index+1] = g1;
-      prev.pixels[index+2] = b1;
-      prev.pixels[index+3] = 255;
-    }
+      // var r1 = video.pixels[index+0];
+      // var g1 = video.pixels[index+1];
+      // var b1 = video.pixels[index+2];
+      //
+      // var r2 = prev.pixels[index+0];
+      // var g2 = prev.pixels[index+1];
+      // var b2 = prev.pixels[index+2];
+      //
+      // if (distSq(r1,g1,b1,r2,g2,b2) > threshold){
+      //   pixels[index+0] = 0;
+      //   pixels[index+1] = 0;
+      //   pixels[index+2] = 0;
+      //
+      // }else{
+      //    pixels[index+0] = 255;
+      //    pixels[index+1] = 255;
+      //    pixels[index+2] = 255;
+      //    pixels[index+3] = 255;
+      //
+      // }
+      //
+      // prev.pixels[index+0] = r1;
+      // prev.pixels[index+1] = g1;
+      // prev.pixels[index+2] = b1;
+      // prev.pixels[index+3] = 255;
+    //}
   }
   updatePixels();
   prev.updatePixels();
