def1daae5f05a7a8aa8fdb17249ac6e6617dac4c
MaxRozhnov <skymax1998@gmail.com>
2017-08-01 14:21:42 -0400

    minor

--- video/sketch.js
+++ video/sketch.js
@@ -7,7 +7,7 @@ function setup() {
   video = createCapture(VIDEO);
   video.size(320,240);
   video.hide();
-  prev = createImage(video.width,video.height*8);
+  prev = createImage(video.width,video.height);
   createCanvas(320,240);
   frameRate(30);
 }
@@ -20,7 +20,7 @@ function draw() {
   for (var y = 0; y < video.height; y++) {
     for (var x = 0; x < video.width; x++) {
       //var index = (video.width - x + 1 + (y * video.width))*4;
-      var index =(y * video.width + x) * 4; 
+      var index =(y * video.width + x) * 4;
 
       var r1 = video.pixels[index+0];
       var g1 = video.pixels[index+1];
